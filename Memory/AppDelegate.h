040d4a5c70d30719bf9bb2af6616ae6fb5e29f23
marian samy <mariansamy@marians-MacBook-Air.local>
2015-03-30 14:39:32 +0200

    Initial Commit

---
+++ Memory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memory
+//
+//  Created by marian samy on 3/30/15.
+//  Copyright (c) 2015 marian samy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memory
+//
+//  Created by marian samy on 3/30/15.
+//  Copyright (c) 2015 marian samy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
